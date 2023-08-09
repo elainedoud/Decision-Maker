53b8472fbeeb0e9b0d1485f5490c248ec1ae52a6
Elaine <elainedoud@gmail.com>
2023-08-09 17:32:49 -0400

    Adding information to create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <Routes>
+    <div className="Homepage">
       <header className="App-header">
       Decision Maker
       </header>
+      <Route path="secondpage">
+      <h4>
+        Welcome
+      </h4>
+      </Route>
     </div>
+    <div className="Secondpage">
+      <Route path="yesno">
+    <h1>Option 1</h1>
+      </Route>
+    </div>
+    <div className="DecidePage">
+      <div>Yes/NoComponent</div>
+        <Route path="/">
+          <div>Return Home</div>
+        </Route>
+    </div>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
