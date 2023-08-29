d20c204d2da65aebe7f334791e445d04be403024
Elaine <elainedoud@gmail.com>
2023-08-29 16:44:26 -0400

    Deleted node modules and package json lock, then reinstalled npm

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,6 @@ import OptionOne from './OptionOne';
 import Homepage from './Homepage';
 import { Routes, Route, BrowserRouter } from "react-router-dom"
 
-require('react-dom')
-window.React2 = require('react')
-console.log(window.React1 === window.React2);
-
 function App() {
 
  return (
