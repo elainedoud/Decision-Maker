c6e4627183d6cb53b099800fc27a62d19b274b2c
Elaine <elainedoud@gmail.com>
2023-08-30 13:12:38 -0400

    Adding React code to create random answer

--- client/src/OptionOne.js
+++ client/src/OptionOne.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function OptionOne() {
+
+    //const [answer, setAnswer] = useState("")
+
+    let answer = true;
+
+    let our_integer = Math.floor(Math.random() * 10)
+    console.log(our_integer)
+
     return (
     <div className="page">
-        <div className="box">
-        <h1>Yes/No</h1>
+        <div className="box"> 
+       {answer?<h1>Yes</h1>:<h1>No</h1>} 
         </div>
     <nav>
-     <Link to="/" className="optionlink">Return Home</Link>
+        <Link to="/" className="optionlink">Return Home</Link>
      </nav>
      </div>
     )
